df97d54d632d5490db5d5c5072ff9b7cc4340aef
Toki1411 <oi-yuko11@i.softbank.jp>
2023-03-02 15:21:21 +0900

    フォントファイルを分割

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,148 +2,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
-
-
 public class PlayerController : MonoBehaviour
 {
-    private int x;
-    public GameObject bird1;
-    public GameObject bird2;
-    public GameObject bird3;
-    public GameObject bird4;
-    public GameObject bird5;
-    public GameObject bird6;
-    public GameObject bird7;
-    public GameObject bird8;
-    public GameObject bird9;
+    public Button[] buttons;
+    public int bombCount;
+    public GameObject gameOverPanel;
+    private List<int> bombIndexList;
+    public GameObject OutPanel;
+    public GameObject SafePanel;
 
-    [SerializeField] GameObject OutPanel;
-    [SerializeField] GameObject SafePanel;
-
-    // Start is called before the first frame update
     void Start()
     {
-
-        x = CountBird.Bird;
-        //xが9の時、０匹の鳥を非アクティブにする
-        if (x == 8)
+        bombIndexList = new List<int>();
+        SetButtonEvents();
+        SetBombs();
+    }
+    // 爆弾をランダムに設置する
+    void SetBombs()
+    {
+        int buttonCount = CountBird.Bird;
+        // Debug.Log(buttonCount);
+        for (int i = 0; i < CountBomb.Bomb; i++)
         {
-            bird9.SetActive(false);
+            int randomIndex = Random.Range(0, buttonCount);
+            // 重複しないようにチェック
+            while (bombIndexList.Contains(randomIndex))
+            {
+                randomIndex = Random.Range(0, buttonCount);
+            }
+            bombIndexList.Add(randomIndex);
         }
-        else if (x == 7)
+    }
+    // 各ボタンにクリックイベントを設定する
+    void SetButtonEvents()
+    {
+        for (int i = 0; i < CountBird.Bird; i++)
         {
-            bird9.SetActive(false);
-            bird8.SetActive(false);
+            int buttonIndex = i;
+            Button button = buttons[i];
+            button.gameObject.SetActive(true);
+            button.onClick.AddListener(() => OnButtonClick(buttonIndex, button));
         }
-        else if (x == 6)
+    }
+    // ボタンがクリックされた時の処理
+    void OnButtonClick(int buttonIndex, Button button)
+    {
+        if (bombIndexList.Contains(buttonIndex))
         {
-            bird9.SetActive(false);
-            bird8.SetActive(false);
-            bird7.SetActive(false);
+            // 爆弾がクリックされた場合
+            button.gameObject.SetActive(false); // ボタンを非アクティブにする（消す）
+            OutPanel.SetActive(true);
+            bombCount++;
+            if(bombCount == bombIndexList.Count)
+            {
+                gameOverPanel.SetActive(true);
+            }
+            //最後の爆弾が押されたとき、ゲーム終了パネルを表示する
+            /*  if (bombIndexList.Contains(buttonIndex) <0)
+              {
+                  gameOverPanel.SetActive(true);
+              }
+           */
         }
-        else if (x == 5)
+        else
         {
-            bird9.SetActive(false);
-            bird8.SetActive(false);
-            bird7.SetActive(false);
-            bird6.SetActive(false);
+            // 爆弾以外がクリックされた場合
+            // button.gameObject.SetActive(false); //消す場合
+            button.interactable = false; // ボタンをクリック不能にする場合
+            SafePanel.SetActive(true);
         }
-        else if (x == 4)
-        {
-            bird9.SetActive(false);
-            bird8.SetActive(false);
-            bird7.SetActive(false);
-            bird6.SetActive(false);
-            bird5.SetActive(false);
-        }
-        else if (x == 3)
-        {
-            bird9.SetActive(false);
-            bird8.SetActive(false);
-            bird7.SetActive(false);
-            bird6.SetActive(false);
-            bird5.SetActive(false);
-            bird4.SetActive(false);
-        }
-        else if (x == 2)
-        {
-            bird9.SetActive(false);
-            bird8.SetActive(false);
-            bird7.SetActive(false);
-            bird6.SetActive(false);
-            bird5.SetActive(false);
-            bird4.SetActive(false);
-            bird3.SetActive(false);
-        }
-        else if (x == 1)
-        {
-            bird9.SetActive(false);
-            bird8.SetActive(false);
-            bird7.SetActive(false);
-            bird6.SetActive(false);
-            bird5.SetActive(false);
-            bird4.SetActive(false);
-            bird3.SetActive(false);
-            bird2.SetActive(false);
-        }
-
-
-    }
-
-    public void CallNiwatori1()
-    {
-        SafePanel.SetActive(true);
-        bird1.SetActive(false);
-    }
-    public void CallNiwatori2()
-    {
-        SafePanel.SetActive(true);
-        bird2.SetActive(false);
-    }
-    public void CallNiwatori3()
-    {
-        SafePanel.SetActive(true);
-        bird3.SetActive(false);
-    }
-    public void CallNiwatori4()
-    {
-        SafePanel.SetActive(true);
-        bird4.SetActive(false);
-    }
-    public void CallNiwatori5()
-    {
-        SafePanel.SetActive(true);
-        bird5.SetActive(false);
-    }
-    public void CallNiwatori6()
-    {
-        SafePanel.SetActive(true);
-        bird6.SetActive(false);
-    }
-    public void CallNiwatori7()
-    {
-        SafePanel.SetActive(true);
-        bird7.SetActive(false);
-    }
-    public void CallNiwatori8()
-    {
-        SafePanel.SetActive(true);
-        bird8.SetActive(false);
-    }
-    public void CallNiwatori9()
-    {
-        SafePanel.SetActive(true);
-        bird9.SetActive(false);
     }
 
     public void RmNiwatori()
-    {
+    { 
         SafePanel.SetActive(false);
     }
-
     public void RmKarage()
-    {
+    { 
         OutPanel.SetActive(false);
     }
 }
